48cd82fec85383e29655c9231f8cfc3474d18838
jakobbbb <jakobbbb@user.noreply.github.com>
2017-04-02 21:06:40 +0200

    cleanup
     Author:    jakobbbb <jakobbbb@user.noreply.github.com>

--- WetterKassel.py
+++ WetterKassel.py
@@ -6,21 +6,26 @@ def main():
     if len(argv) != 2:
         print >> stderr, "[-] Wrong amount of arguments supplied." 
         exit(1)
-    if argv[1] == "morning":
-        tweettime = 0 # current day
-    elif argv[1] == "evening":
-        tweettime = 1 #next day
+    if argv[1] == "today":
+        tweettime = 0 
+    elif argv[1] == "tomorrow":
+        tweettime = 1
     else: 
         print >> stderr, "[-] Wrong argument(s) supplied." 
         exit(1)
         
+    # Get Data from Weather API
     wapi = WetterKassel.weatherapi()
     data = wapi.getdata(dummy=False) # get json weather data
     print "[+][%s] Got Weather Data" % str(time())
+    
+    # process data and build tweet
     daydata = data["daily"]["data"][tweettime]
     tweetbuilder = WetterKassel.tweetbuild(data=daydata, tweettime=tweettime)
     tweet = tweetbuilder.build()
     print "[+][%s] Built Tweet" % str(time())
+
+    # post tweet
     tapi = WetterKassel.twitterapi()
     tapi.post(tweet.encode("utf-8")) # encode to fix issue under linux
     print "[+][%s] Sent Tweet" % str(time())
